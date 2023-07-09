3ea1941247ee864e22fdcf167afdb46152998b31
Abulabn Belal <76918001+BelalAbulabn@users.noreply.github.com>
2023-07-09 15:12:46 +0200

    Update Api.py

--- Api.py
+++ Api.py
@@ -3,7 +3,7 @@ import tweepy
 
 # Authenticate to Twitter using the API key and secret
 auth = tweepy.OAuth1UserHandler("", "")
-auth.set_access_token("-4vxRxOJ9UaNaKxXiS2cIBmxg28Wnh7", "")
+auth.set_access_token("-4vxRxOJ9UaNaKxXiS2nh7", "")
 
 # Create API object
 api = tweepy.API(auth)
